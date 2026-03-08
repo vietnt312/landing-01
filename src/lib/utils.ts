3cc2fd7c23f7b0fbafae1a0f3fdf01e3ae9a29ea
vincent <vietnt312@gmail.com>
2026-03-09 02:26:54 +0700

    update img path

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,10 +2,10 @@ export const base = import.meta.env.BASE_URL;
 
 export function imgSrc(src: string): string {
     if (src.startsWith("http")) return src;
-    
+
     // Ensure base doesn't have a trailing slash and src doesn't have a leading slash
     const b = base.replace(/\/$/, "");
     const s = src.replace(/^\//, "");
-    
+
     return `${b}/${s}`;
 }
