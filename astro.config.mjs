816ae4e7d4c716e9ff9ceda934d84c1e4e016a42
nguyentrongviet <nguyentrongviet@luvina.net>
2026-03-17 23:35:19 +0700

    update config + fix base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://achub.site",
+  site: "https://www.achub.site",
   // base: "/landing-01",
   integrations: [tailwind()],
   output: "static",
